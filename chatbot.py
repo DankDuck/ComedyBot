26ead4741c66973b617a83340191f72c60878356
Regis Gurung <125932827+DankDuck@users.noreply.github.com>
2025-05-29 18:03:32 -0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,8 +3,10 @@ from jokes import raw_jokes, joke_vectors
 from scipy.spatial.distance import cosine
 import spacy
 import time
+
 import numpy as np
 np.seterr(divide='ignore', invalid='ignore')
+
 nlp = spacy.load('en_core_web_md')
 
 class ComedyBot():
@@ -18,6 +20,7 @@ class ComedyBot():
 
 # starting greeting
    def greet(self):
+       print("I'm Comedy Bot (°▽°)/	 ")
        response = input("What topic would you like your joke on? ")
        self.give_funny(response)
 
@@ -34,18 +37,19 @@ class ComedyBot():
        while not self.make_exit(topic_word):
            most_similar_index = self.find_most_similar_index(topic_word)
            if most_similar_index:
-            print(raw_jokes[most_similar_index])
+            print('\n' + raw_jokes[most_similar_index] + '\n')
             # 1s delay
             time.sleep(1)
-            topic_word = input("What's another topic you would a joke on? ")
+            topic_word = input("What's another topic you would a joke on? (◕‿◕)\n	")
         # if no similar joke found
            else:
                time.sleep(1)
-               print("Joke not found! (˚ ˃̣̣̥⌓˂̣̣̥ )")
-               topic_word = input("What's another topic you would a joke on? ")
+               print('\n' + "Joke not found o(〒﹏〒)o	" + '\n')
+               topic_word = input("What's another topic you would a joke on? (◕‿◕)\n ")
 
 # use word2vec to find closest word to topic word
    def find_most_similar_index(self, topic_word):
+       topic_word = topic_word.split()[0]
        topic_word_vector = nlp(topic_word).vector
        most_similar_dist = 100
        curr_index = 0
